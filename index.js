61c9c5539ed0542b98ca54c667da2b2fc7079f4a
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-12 15:03:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // write your code below!
-function happyHolidays() {
+function happyHolidays(string) {
   return "Happy holidays!"
 }
-function happyHolidaysTo() {
+function happyHolidaysTo(string) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidayTo() {
+function happyHolidayTo(string) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown() {
+function holidayCountdown(string) {
   return `It's ${days} days until ${holiday}!`
 }
